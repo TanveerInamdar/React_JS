cf4c80ab84e1392c598f55413c5a3bcd77632940
Tanveer Inamdar <tinamdar@ttu.edu>
2025-11-26 17:58:14 -0600

    Starting off

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+
+function Header(){ //React functions can only return one element. Here an element is <header> but we can add sub elements.
+    return(
+        <header>
+            <h1>My Website</h1>
+
+        </header>
+    );
+}
+
+export default Header
